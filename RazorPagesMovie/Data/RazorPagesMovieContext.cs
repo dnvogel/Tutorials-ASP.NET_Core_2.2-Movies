f5463aa20616aa547442ccb0e6cc8c7f4b01c428
David Vogel <dnvogel@gmail.com>
2019-02-16 19:24:52 -0500

    Initial Commit, following tutorial for Microsoft asp.net core 2.2 - added Movie model and data context for Movie CRUD operations

---
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        // The connection string is passed in the options
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+        
+        // An Entity Set typically corresponds to a database table. An entity corresponds to a row in the table.
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
