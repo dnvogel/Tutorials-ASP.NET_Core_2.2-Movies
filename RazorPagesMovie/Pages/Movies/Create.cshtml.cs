c2d544e6f657b4ef34ab4e9ef2ecfee9749a0425
David Vogel <dnvogel@gmail.com>
2019-02-16 20:21:19 -0500

    Removing unused usings, adding comments to the Move Create page

--- RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages.Movies
 {
     public class CreateModel : PageModel
     {
-        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+        private readonly RazorPagesMovieContext _context;
 
-        public CreateModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        public CreateModel(RazorPagesMovieContext context)
         {
             _context = context;
         }
@@ -23,9 +19,16 @@ namespace RazorPagesMovie.Pages.Movies
             return Page();
         }
 
+        /// <summary>
+        /// The Movie property uses the BindProperty attribute to opt-in to model binding. 
+        /// When the Create form posts the form values, the ASP.NET Core runtime binds the posted values to the Movie model.
+        /// </summary>
         [BindProperty]
         public Movie Movie { get; set; }
 
+        /// <summary>
+        /// The OnPostAsync method is run when the page posts form data.
+        /// </summary>
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
