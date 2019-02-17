5409a6c90c6e9d7ed7ac5c1c6753bf55f37c1cd4
David Vogel <dnvogel@gmail.com>
2019-02-16 21:53:03 -0500

    Adding searching functionality to Movies for Genre and Title

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -30,6 +31,24 @@ namespace RazorPagesMovie.Pages.Movies
         public IList<Movie> Movie { get;set; }
 
         /// <summary>
+        /// Search String
+        /// </summary>
+        /// <note>
+        /// For security reasons, you must opt in to binding GET request data to page model properties.
+        /// Verify user input before mapping it to properties. 
+        /// Opting in to GET binding is useful when addressing scenarios which rely on query string or route values.
+        /// To bind a property on GET requests, set the [BindProperty] attribute's SupportsGet property to true:
+        /// </note>        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        /// <summary>
         /// Handles requests asynchronously and reutrns a list movies to the Razor Page
         /// </summary>
         /// <note>
@@ -40,7 +59,29 @@ namespace RazorPagesMovie.Pages.Movies
         /// <returns><see cref="Task"/></returns>
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            // The SlectList of genres is created by projecting the distinct genres.
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            
+            Movie = await movies.ToListAsync();
         }
     }
 }
