0990c4d46c4b0474c744d5b0d998cf3970ea5de9
David Vogel <dnvogel@gmail.com>
2019-02-16 20:04:27 -0500

    Adding comments, changing title, removing area from index. The area isn't needed for now, so commenting it out. See https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/areas?view=aspnetcore-2.2 for more information on Areas.

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -12,7 +12,16 @@ namespace RazorPagesMovie.Pages.Movies
     public class IndexModel : PageModel
     {
         private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
-
+               
+        /// <summary>
+        /// Constructor for IndexModel
+        /// </summary>
+        /// <note>
+        /// Razor Pages are derived from PageModel. 
+        /// By convention, the PageModel-derived class is called <PageName>Model. 
+        /// The constructor uses dependency injection to add the RazorPagesMovieContext to the page.
+        /// </note>
+        /// <param name="context"></param>
         public IndexModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
         {
             _context = context;
@@ -20,6 +29,15 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        /// <summary>
+        /// Handles requests asynchronously and reutrns a list movies to the Razor Page
+        /// </summary>
+        /// <note>
+        /// When a request is made for the page, the OnGetAsync method returns a list of movies to the Razor Page.
+        /// OnGetAsync or OnGet is called on a Razor Page to initialize the state for the page.
+        /// In this case, OnGetAsync gets a list of movies and displays them.
+        /// </note>
+        /// <returns><see cref="Task"/></returns>
         public async Task OnGetAsync()
         {
             Movie = await _context.Movie.ToListAsync();
